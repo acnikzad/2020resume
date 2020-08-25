2d36dafc21fc4eb931cdf16f5d08d43bbdab3d2d
“Aria <a.c.nikzad@gmail.com>
2020-08-25 13:23:43 -0700

    working on new section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,30 @@
 
 <template>
   <div id="app">
-    <section class="section-profile">
-      <div class="col span-4-of-12">
-        <h3>Currently</h3>
+    <section class="section-projects">
+      <div class="row">
+        <div class="project-box">
+          <h2>Portfolio</h2>
+        </div>
       </div>
-      <div class="col span-8-of-12 timeline">
-        <p>Looking searching for any projects big or small</p>
-        <br>
-        <br>
-        <br>
+      <div class="row">
+        <div class="col span-1-of-3 project-box">
+          <h3>Projects</h3>
+          <p>BlockHound</p>
+        </div>
+        <div class="col span-1-of-3 project-box">
+          <h3>Paid Projects</h3>
+          <p>Concrete Landscape Services</p>
+        </div>
+        <div class="col span-1-of-3 project-box">
+          <h3>Currently Working On</h3>
+          <ul>
+            <li><a href="#">Kirbys Trailer Works</a></li>
+            <li><a href="#">Christoffersen Collection</a></li>
+            <li><a href="#">True Reflections Detailing</a></li>
+          </ul>
+        </div>
       </div>
-      <img src="../../public/assets/images/dot.png" class="circle">
     </section>
   </div>
 <!--     <section class="name">
