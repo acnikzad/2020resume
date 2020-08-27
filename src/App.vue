1863ab34383627cebf30d5658caee2daa73cb890
“Aria <a.c.nikzad@gmail.com>
2020-08-27 11:04:32 -0700

    added css

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,8 @@
               <i class="devicon-css3-plain"></i>
               <i class="devicon-html5-plain"></i>
               <i class="devicon-bootstrap-plain"></i>
-              <i class="devicon-heroku-original"></i>
               <i class="devicon-github-plain"></i>
+              <i class="ion-social-bitcoin"></i>
             </div>
           </div>
           <h1></h1>
@@ -39,10 +39,33 @@
       </section>
     </header>
     <router-view/>
-
-    <footer id="footer" class="midnight-blue">
-        <!-- ... -->
-    </footer><!--/#footer-->
+    <footer>
+      <div class="row">
+        <div class="col span-1-of-3">
+          <ul class="footer-nav">
+            <li>
+              <a href="#" class="footer-icon-small"><i class="ion-email"></i></a>
+              <p>a.c.nikzad@gmail.com</p>
+            </li>
+          </ul>
+        </div>
+        <div class="col span-1-of-3">
+          <ul class="social-links">
+            <li><a href="#" class="footer-icon-small"><i class="ion-map"></i></a></li>
+            <li>Sacramento, CA</li>
+          </ul>
+        </div>
+        <div class="col span-1-of-3">
+          <ul class="social-links">
+            <li><a href="#"><i class="footer-icon-iphone"></i></a></li>
+            <li>(415) 328-7478</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <p>"The same boiling water that softens the potato hardens the egg. <br>It's about what you're made of, not the circumstances."<br>- Mel Robbins</p>
+      </div>
+    </footer>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,29 +2,43 @@
 <template>
   <div id="app">
     <section class="section-projects">
-      <div class="border-box">
-        <div class="row">
-          <div class="project-box">
-            <h2>Portfolio</h2>
-            <hr>
-          </div>
-        </div>
+      <div class="section-projects-header">
+        <h2>Portfolio</h2>
+      </div>
+      <div class="portfolio-box">
         <div class="row">
           <div class="col span-1-of-3 project-box">
+            <i class="ion-code"></i>
             <h3>Projects</h3>
-            <p>BlockHound</p>
+            <div class="tiles">
+              <article class="tile-text">
+                <img src="../../public/assets/images/BHMap.jpg">
+                <a href="#">
+                  <h2 clss="centered">Blockhound</h2>
+                </a>
+              </article>
+              <img src="../../public/assets/images/Blue2.jpg">
+            </div>
           </div>
+
+
+
+
           <div class="col span-1-of-3 project-box">
             <h3>Paid Projects</h3>
-            <p>Concrete Landscape Services</p>
+            <div class="tiles">
+              <img src="../../public/assets/images/CLS.jpg">
+            </div>
           </div>
           <div class="col span-1-of-3 project-box">
             <h3>Currently Working On</h3>
-            <ul>
-              <li><a href="#">Kirbys Trailer Works</a></li>
-              <li><a href="#">Christoffersen Collection</a></li>
-              <li><a href="#">True Reflections Detailing</a></li>
-            </ul>
+            <div class="tiles">
+              <ul>
+                <img src="../../public/assets/images/CC.jpg">
+                <li><a href="#">Christoffersen Collection</a></li>
+                <li><a href="#">True Reflections Detailing</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
