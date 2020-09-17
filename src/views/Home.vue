a7d30a1ee5e974dda874d1b7055ac3a84f74224b
“Aria <a.c.nikzad@gmail.com>
2020-09-17 12:03:36 -0700

    worked on education

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 
 <template>
   <div id="app">
+
     <section class="section-projects">
-      <div class="section-projects-header">
+      <div class="section-header">
         <h1>Portfolio</h1>
         <p class="">&nbsp;</p>
       </div>
@@ -17,6 +18,7 @@
                   <p>Blockhound</p>
                 </a>
               </article>
+              <br>
               <article class="tile-text">
                 <a href="#">
                   <img src="../../public/assets/images/Blue2.jpg">
@@ -50,19 +52,52 @@
         </div>
       </div>
     </section>
-
     <section class="section-education">
-      <div class="row">
-        
+      <div class="section-header">
+        <h1>Education</h1>
+        <p class="">&nbsp;</p>
+      </div>
+      <br>
+      <br>
+      <br>
+      <div class="education-box">
+        <div class="row span-1-of-1">
+          <div class="col span-1-of-2 box schools">
+            <h3>San Francisco State University</h3>
+          </div>
+          <div class="col span-1-of-2 box schools">
+            <!-- <img src="../../public/assets/images/17640.svg" class="education-icon"> -->
+            <h3>Actualize Coding Bootcamp</h3>
+              <div class="row">
+                <div class="col span-1-of-2 box">
+                  <h5>Frameworks</h5>
+                  <ul class="education-list">
+                    <li>Rails</li>
+                    <li>Vue.js</li>
+                    <li>React</li>
+                    <li></li>
+                    <li></li>                
+                  </ul>
+                </div>
+                <div class="col span-1-of-2 box">
+                  <h5>Languages</h5>
+                  <ul class="education-list">
+                    <li>Ruby</li>
+                    <li>Javascript</li>
+                    <li>HTML/CSS</li>
+                    <li></li>                
+                  </ul>
+                </div>
+              </div>
+          </div>
+        </div>
       </div>
     </section>
-
-
-
   </div>
 
 </template>
 
 <script>
+
 
 </script>
